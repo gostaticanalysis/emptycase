730c4956dfe61fb6e5eaae4b4a57c1ea73286e27
KamikazeZirou <kamikaze.zirou@gmail.com>
2021-05-11 23:09:06 +0900

    first commit

---
+++ emptycase_test.go
@@ -0,0 +1,15 @@
+package emptycase_test
+
+import (
+	"testing"
+
+	"emptycase"
+	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, emptycase.Analyzer, "a")
+}
